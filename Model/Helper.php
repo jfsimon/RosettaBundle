87866d8bb047fe365103afd8084fb9cfe3924400
Jean-François Simon <contact@jfsimon.fr>
2011-08-21 11:22:52 +0200

    [model] added entity helper

---
+++ Model/Helper.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace BeSimple\RosettaBundle\Model;
+
+/**
+ * Helper class for message texts.
+ *
+ * @author: Jean-François Simon <contact@jfsimon.fr>
+ */
+class Helper implements HelperInterface
+{
+    /**
+     * @var \Closure
+     */
+    private $hasher;
+
+    /**
+     * @var \Closure
+     */
+    private $keyGuesser;
+
+    /**
+     * @var \Closure
+     */
+    private $choiceGuesser;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->hasher = function($text) {
+            // sha1 + md5 to avoid collisions
+            // raw encoded: 20 + 16 chars only
+            return sha1($text, true).md5($text, true);
+        };
+
+        $this->keyGuesser = function($text) {
+            // no white and at least one dot
+            return !preg_match('/\s/', $text) && strpos($text, '.');
+        };
+
+        $this->choiceGuesser = function($text) {
+            // just looks for a pipe
+            return strpos($text, '|');
+        };
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hash($text)
+    {
+        $closure = $this->hasher;
+
+        return $closure($text);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isKey($text)
+    {
+        $closure = $this->keyGuesser;
+
+        return $closure($text);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isChoice($text)
+    {
+        $closure = $this->choiceGuesser;
+
+        return $closure($text);
+    }
+
+    /**
+     * @param \Closure $choiceGuesser
+     *
+     * @return Helper
+     */
+    public function setChoiceGuesser(\Closure $choiceGuesser)
+    {
+        $this->choiceGuesser = $choiceGuesser;
+
+        return $this;
+    }
+
+    /**
+     * @return \Closure
+     */
+    public function getChoiceGuesser()
+    {
+        return $this->choiceGuesser;
+    }
+
+    /**
+     * @param \Closure $hasher
+     *
+     * @return Helper
+     */
+    public function setHasher(\Closure $hasher)
+    {
+        $this->hasher = $hasher;
+
+        return $this;
+    }
+
+    /**
+     * @return \Closure
+     */
+    public function getHasher()
+    {
+        return $this->hasher;
+    }
+
+    /**
+     * @param \Closure $keyGuesser
+     *
+     * @return Helper
+     */
+    public function setKeyGuesser(\Closure $keyGuesser)
+    {
+        $this->keyGuesser = $keyGuesser;
+
+        return $this;
+    }
+
+    /**
+     * @return \Closure
+     */
+    public function getKeyGuesser()
+    {
+        return $this->keyGuesser;
+    }
+}
---
+++ Model/HelperInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BeSimple\RosettaBundle\Model;
+
+/**
+ * Interface message texts helpers.
+ *
+ * @author: Jean-François Simon <contact@jfsimon.fr>
+ */
+interface HelperInterface
+{
+    /**
+     * Returns a hash of given text.
+     *
+     * @param $text A text to hash
+     *
+     * @return string The hash
+     */
+    function hash($text);
+
+    /**
+     * Returns true if given text looks like a translation key.
+     *
+     * @param $text A text to translate
+     *
+     * @return bool Is given text a translation key
+     */
+    function isKey($text);
+
+    /**
+     * Return true if given text looks like message choices.
+     *
+     * @param $text A text to translate
+     *
+     * @return bool Is given text choices
+     */
+    function isChoice($text);
+}
