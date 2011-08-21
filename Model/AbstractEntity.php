4c8ea4b1b5f6a238977f9794ee32f00ff07a54a6
Jean-François Simon <contact@jfsimon.fr>
2011-08-21 11:22:19 +0200

    [model] added base entity class

---
+++ Model/AbstractEntity.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace BeSimple\RosettaBundle\Model;
+
+/**
+ * @author: Jean-François Simon <contact@jfsimon.fr>
+ */
+abstract class AbstractEntity
+{
+    /**
+     * @var mixed
+     */
+    protected $id;
+
+    /**
+     * @var \DateTime
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    protected $updatedAt;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $updated_at
+     *
+     * @return AbstractEntity
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @return AbstractEntity
+     */
+    public function notifyUpdate()
+    {
+        $this->updatedAt = new \DateTime();
+
+        return $this;
+    }
+}
