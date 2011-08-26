20cffae549576079b6bb9a53321ea9e9dd838776
Jean-François Simon <contact@jfsimon.fr>
2011-08-26 12:53:43 +0200

    [core] added (empty) dumper test

---
+++ Tests/Rosetta/DumperTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BeSimple\RosettaBundle\Tests\Rosetta;
+
+use BeSimple\RosettaBundle\Tests\WebTestCase;
+use Symfony\Component\HttpKernel\Util\Filesystem;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * @author: Jean-François Simon <contact@jfsimon.fr>
+ */
+class DumperTest extends WebTestCase
+{
+    // As for locator, tests will come as soon
+    // as I will figure how to build them!
+}
