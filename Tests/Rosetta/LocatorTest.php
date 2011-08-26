903823a678e4dddc2a3be1c5078e7975520e87ce
Jean-François Simon <contact@jfsimon.fr>
2011-08-26 07:41:29 +0200

    [core] added locator (empty) test

---
+++ Tests/Rosetta/LocatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BeSimple\RosettaBundle\Tests\Rosetta;
+
+use BeSimple\RosettaBundle\Tests\WebTestCase;
+
+/**
+ * @author: Jean-François Simon <contact@jfsimon.fr>
+ */
+class LocatorTest
+{
+    // As locator is highly linked to framework standards,
+    // I don't really know how to test it for now.
+    // I'm working on it!
+}
